29688f14600f4380ebf6eccbc16ac5b9e4fdfdaf
5SUPERMAN <53365021+5SUPERMAN@users.noreply.github.com>
2019-09-07 21:36:13 +0800

    'RecommendView'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,13 +3,15 @@
     <nav-bar class="home-nav">
       <div slot="center">购物车</div>
     </nav-bar>
-    <home-swiper :banners="banners"/>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
 
 import { getHomeMultidata } from "network/home.js";
 
@@ -30,7 +32,8 @@ export default {
   },
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
   }
 };
 </script>
